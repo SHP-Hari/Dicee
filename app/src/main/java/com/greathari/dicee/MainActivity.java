1e19548f45996e36b5b26262d00844d03dc3ea2f
SHP-Hari <greathari777@gmail.com>
2019-05-05 01:20:56 +0530

    Dicee Android Application inial Commit

---
+++ app/src/main/java/com/greathari/dicee/MainActivity.java
@@ -0,0 +1,52 @@
+package com.greathari.dicee;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        final ImageView leftDice;
+        final ImageView rightDice;
+
+
+        rollButton = (Button) findViewById(R.id.btnRoll);
+        leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        rightDice = (ImageView) findViewById(R.id.image_rightDice);
+        final int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6,
+        };
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumberGenerator = new Random();
+                int number  = randomNumberGenerator.nextInt(6);
+//                Log.d("Dicee", "Roll Button has been clicked");
+//                Log.d("Dicee", "The Number generated is  : "+number);
+//                Toast.makeText(getApplicationContext(),"Clicked"+number, Toast.LENGTH_LONG).show();
+                leftDice.setImageResource(diceArray[number]);
+                number = randomNumberGenerator.nextInt(6);
+                rightDice.setImageResource(diceArray[number]);
+            }
+        });
+    }
+}
